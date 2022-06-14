79104085eb8e63ee80edaf38007fd4a755a4c39f
Etienne SANDILLON <etienne.sandillon971@orange.fr>
2022-06-14 10:12:27 +0200

    webfront

---
+++ page/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<HTML>
+	<head>
+		<meta charset="utf-8">
+		<title>Animation login form</title>
+		<link rel="stylesheet" type="text/css" href="../css/login.css">
+
+	</head>
+
+<body>
+	<form class="box" action="index.html" method="post">
+		<h1>Login</h1>
+
+    </div>
+		<input type="text" name="" placeholder="Username">
+		<input type="Password" name="" placeholder="Password">
+		    <p class="inscription">Je n'ai pas de <span>compte</span>. Je m'en <span>crée</span> un.</p>
+    <div align="center">
+		<input type="submit" name="" value="Connecting">
+
+	</form>
+	
+</body>
+
+
+
+
+</HTML>
